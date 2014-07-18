2806d30f06da67d6c62cd5da85385c02e64888d7
Vladimir <vladimirkevorkov2@yandex.ru>
2014-07-18 20:27:26 +0300

    Init
    Add files

---
+++ keyfuck.js
@@ -0,0 +1,43 @@
+function run() {
+	var str=document.getElementById('key').value;
+	lets(str);
+}
+function getCats(smth) {
+	//translates simple text to keyfuck text 	
+}
+
+function lets(str) {
+	var st="";
+	var car=[];
+	var max_val=29999;
+	for (var i=0;i<=max_val;i++) car[i]=0;
+	var dig=0;
+	var keys=['+','-','>','<','.','@']; //array of using chars
+	for (var x=0;x<str.length;x++) {
+		switch (str.charAt(x)) {
+			case keys[0]:
+			car[dig]++;
+			break;
+				case keys[1]:
+				car[dig]--;
+				break;
+					case keys[2]:
+					dig>max_val ? dig=0 : dig++;
+					break;
+						case keys[3]:
+						dig===0 ? dig=max_val : dig--;
+						break;
+							case keys[4]:
+								st+=String.fromCharCode(car[dig]);
+				   			  break;
+								case keys[5]:
+								car[dig]=0;
+								break;
+									default:
+									wrt("I dont know what the hell you are writing here");
+										break;
+		}
+		
+	}
+	document.getElementById('out').innerHTML=st;	//output		
+}
