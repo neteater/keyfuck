8e08348b35069111f9e031b9244337d0bcec4cb8
Vladimir <vladimirkevorkov2@yandex.ru>
2014-07-19 13:43:41 +0300

    #1
    1. Cell limit
    2. Bug fix

--- keyfuck.js
+++ keyfuck.js
@@ -5,7 +5,9 @@ function run() {
 function getCats(smth) {
 	//translates simple text to keyfuck text 	
 }
-
+function out(text) {
+document.getElementById('out').innerHTML=text;
+}
 function lets(str) {
 	var st="";
 	var car=[];
@@ -16,10 +18,10 @@ function lets(str) {
 	for (var x=0;x<str.length;x++) {
 		switch (str.charAt(x)) {
 			case keys[0]:
-			car[dig]++;
+			car[dig]>255 ? car[dig]=0 : car[dig]++;
 			break;
 				case keys[1]:
-				car[dig]--;
+				car[dig]===0 ? car[dig]=0 : car[dig]--;
 				break;
 					case keys[2]:
 					dig>max_val ? dig=0 : dig++;
@@ -34,10 +36,10 @@ function lets(str) {
 								car[dig]=0;
 								break;
 									default:
-									wrt("I dont know what the hell you are writing here");
+									out("I dont know what the hell you are writing here");
 										break;
 		}
-		
 	}
-	document.getElementById('out').innerHTML=st;	//output		
+
+		out(st);//output		
 }
